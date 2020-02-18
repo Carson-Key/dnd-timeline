76501f775d745e6005d552826efd1e05c70da68b
Carson-Key <keyc@oregonstate.edu>
2020-02-18 12:36:53 -0800

    Refactor to be more module

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div>
-        <br />
         <Time timeline={timeline} />
       </div>
     )
--- src/containers/Time/index.js
+++ src/containers/Time/index.js
@@ -6,23 +6,30 @@ class Time extends Component {
   render() {
     const { timeline } = this.props
     const events = Object.keys(timeline)
-    return events.map((event, i) => {
-      return (
-        <center key={i}>
-          <Media>
-            <Media body>
-              <Media heading>
-                {timeline[event].date}
-              </Media>
-              {timeline[event].description}
-            </Media>
-          </Media>
-          <br/>
-          <div className="line"></div>
-          <br/>
-        </center>
-      )
-    })
+    return (
+      <div>
+      <br />
+        {
+          events.map((event, i) => {
+            return (
+              <center key={i}>
+                <Media>
+                  <Media body>
+                    <Media heading>
+                      {timeline[event].date}
+                    </Media>
+                    {timeline[event].description}
+                  </Media>
+                </Media>
+                <br/>
+                <div className="line"></div>
+                <br/>
+              </center>
+            )
+          })
+        }
+      </div>
+    )
   }
 }
 
