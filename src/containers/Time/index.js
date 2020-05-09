eef47bd3f4139589c1bc079f8fb560d96ebe0518
Carson-Key <keyc@oregonstate.edu>
2020-05-09 15:38:05 -0700

    Time component will now accept the new timeline object format

--- src/containers/Time/index.js
+++ src/containers/Time/index.js
@@ -6,39 +6,41 @@ class Time extends Component {
   render() {
     const { timeline } = this.props
     const events = Object.keys(timeline).sort((a, b) => {
-      return +a - +b
+      return +parseInt(a) - +parseInt(b)
     })
     return (
       <div>
       <br />
         {
-          events.map((event, i) => {
-            var dateNumber = timeline[event].date
+          events.map((year, i) => {
+            var dateNumber = parseInt(year)
             var date
             if (dateNumber < 0) {
-              date = Math.abs(timeline[event].date)
+              date = Math.abs(dateNumber)
               date = date.toString()
               date = date + " " + this.props.lessThanYearZero
             } else if (dateNumber >= 0) {
-              date = Math.abs(timeline[event].date)
+              date = Math.abs(dateNumber)
               date = date.toString()
               date = date + " " + this.props.greaterThanYearZero
             }
-            return (
-              <center key={i}>
-                <Media>
-                  <Media body>
-                    <Media heading>
-                      {date}
+            return timeline[year].map((event, j) => {
+              return (
+                <center key={j}>
+                  <Media>
+                    <Media body>
+                      <Media heading>
+                        {date}
+                      </Media>
+                      {event.description}
                     </Media>
-                    {timeline[event].description}
                   </Media>
-                </Media>
-                <br/>
-                <div className="line"></div>
-                <br/>
-              </center>
-            )
+                  <br/>
+                  <div className="line"></div>
+                  <br/>
+                </center>
+              )
+            })
           })
         }
       </div>
