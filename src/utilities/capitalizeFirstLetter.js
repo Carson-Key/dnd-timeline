38dd7024c3202053341f9f0dd70cb47ddc86beab
Carson-Key <keyc@oregonstate.edu>
2020-05-25 20:14:27 -0700

    capitalizeFirstLetter now handles null/empty strings

--- src/utilities/capitalizeFirstLetter.js
+++ src/utilities/capitalizeFirstLetter.js
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(string) {
-  return string[0].toUpperCase() + string.slice(1)
+  if (string === "" || string === null || string === undefined) {
+    return ""
+  } else {
+    return string[0].toUpperCase() + string.slice(1)
+  }
 }
