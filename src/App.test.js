e6e06d255ba79ce5e7c7b54981b90118232c4d95
Carson-Key <keyc@oregonstate.edu>
2020-05-25 18:16:24 -0700

    Add snapshot tests to Time

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,33 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Renders Auth', () => {
+  it('renders title', () => {
+    const { getByText } = render(<App />);
+    const titleElement = getByText(/Timeline/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+})
+
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import { unmountComponentAtNode } from "react-dom";
+// import Time from './Time';
+//
+// beforeEach(() => {
+//   const { Time } = render(<Time  />);
+// });
+//
+// afterEach(() => {
+//   // cleanup on exiting
+//   unmountComponentAtNode(container);
+//   container.remove();
+//   container = null;
+// });
+//
+// test('Renders Time', () => {
+//   describe('', () => {
+//
+//   })
+// })
---
+++ src/containers/Time/Time.test.js
@@ -0,0 +1,127 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Time from "./index.js";
+import renderer from 'react-test-renderer';
+
+describe('Render Time ->', () => {
+
+  test('with blank timeline prop', () => {
+    const component = renderer.create(
+      <Time timeline={{}}/>,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with out year suffix timeline prop', () => {
+    const component = renderer.create(
+      <Time timeline={{
+        "1776": [
+          {
+            description: "America!!!",
+          }
+        ]
+      }}/>,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with events and year suffix timeline prop', () => {
+    const component = renderer.create(
+      <Time
+        timeline={{
+          "1776": [
+            {
+              description: "America!!!",
+            }
+          ]
+        }}
+        lessThanYearZero="BCE"
+        greaterThanYearZero="AD"
+      />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with multiple years', () => {
+    const component = renderer.create(
+      <Time
+        timeline={{
+          "1776": [
+            {
+              description: "America!!!",
+            }
+          ],
+          "1777": [
+            {
+              description: "More America!!!",
+            }
+          ]
+        }}
+        lessThanYearZero="BCE"
+        greaterThanYearZero="AD"
+      />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with multiple events', () => {
+    const component = renderer.create(
+      <Time
+        timeline={{
+          "1776": [
+            {
+              description: "America!!!",
+            },
+            {
+              description: "America 2.0!!!",
+            }
+          ]
+        }}
+        lessThanYearZero="BCE"
+        greaterThanYearZero="AD"
+      />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('with multiple events and years', () => {
+    const component = renderer.create(
+      <Time
+        timeline={{
+          "1776": [
+            {
+              description: "America!!!",
+            },
+            {
+              description: "America 2.0!!!",
+            }
+          ],
+          "1777": [
+            {
+              description: "More America!!!",
+            },
+            {
+              description: "More America 2.0!!!",
+            }
+          ]
+        }}
+        lessThanYearZero="BCE"
+        greaterThanYearZero="AD"
+      />,
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+})
