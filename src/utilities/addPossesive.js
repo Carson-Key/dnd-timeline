823596f31b32fae4e0ead8b25660ebcb826f3c03
Carson-Key <keyc@oregonstate.edu>
2020-05-25 20:20:40 -0700

    addPossesive now handles blank/null strings

--- src/utilities/addPossesive.js
+++ src/utilities/addPossesive.js
@@ -1,5 +1,8 @@
 export function addPossesive(string) {
-  if (string[string.length - 1] === 's') {
+  if (string === "" || string === null || string === undefined) {
+    return ""
+  }
+  else if (string[string.length - 1] === 's') {
     return string + "'"
   } else {
     return string + "'s"
