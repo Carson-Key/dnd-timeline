70472118cc1d27a240a55a9402150ba331258feb
Carson-Key <keyc@oregonstate.edu>
2020-06-25 19:40:20 -0700

    Updates

---
+++ precache-manifest.2ab69620f56214aef8939f336e5445e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ed26aaa7dd7957da8e6f0c94606d9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78377c4f09e90fba2aaf",
+    "url": "/static/css/main.227a9ac0.chunk.css"
+  },
+  {
+    "revision": "8c2c3e2b5c2433a69aff",
+    "url": "/static/js/2.50839f91.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.50839f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78377c4f09e90fba2aaf",
+    "url": "/static/js/main.14c22c22.chunk.js"
+  },
+  {
+    "revision": "0c176be66ec848c8d666",
+    "url": "/static/js/runtime-main.7a29fc11.js"
+  }
+]);
