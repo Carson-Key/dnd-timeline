78f9c30b2b55628d5e6d4105fc35c3f968628c54
Carson-Key <keyc@oregonstate.edu>
2020-06-25 19:41:19 -0700

    Updates

---
+++ precache-manifest.ae3878c7f6929c153672842c4c15300a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d8face49caf05af382dd9545f36e20",
+    "url": "/dnd-timeline/index.html"
+  },
+  {
+    "revision": "e7d8752290607ae7f360",
+    "url": "/dnd-timeline/static/css/main.227a9ac0.chunk.css"
+  },
+  {
+    "revision": "8c2c3e2b5c2433a69aff",
+    "url": "/dnd-timeline/static/js/2.50839f91.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/dnd-timeline/static/js/2.50839f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7d8752290607ae7f360",
+    "url": "/dnd-timeline/static/js/main.0b160483.chunk.js"
+  },
+  {
+    "revision": "9f014ab8f66e2e3c55f9",
+    "url": "/dnd-timeline/static/js/runtime-main.08b25a62.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ab69620f56214aef8939f336e5445e0.js"
+  "/dnd-timeline/precache-manifest.ae3878c7f6929c153672842c4c15300a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dnd-timeline/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
